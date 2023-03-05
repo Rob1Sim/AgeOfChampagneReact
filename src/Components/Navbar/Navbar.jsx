a2c1ff323a8df9c8533873dfce4e682d8d21bcb4
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-05 15:31:11 +0100

    Restructuratio du css et de l'html pour correspondre au mobile first

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,47 +2,52 @@
 import  logo  from "/public/images/favicon/favicon-32x32.png"
 import  franceFlag  from "/public/images/flags/france.png"
 import  usFlag  from "/public/images/flags/united-states.png"
+import {useState} from "react";
 
 function Navbar(){
+    const [showLinks, setShowLinks] = useState(false)
+
+    const handleShowLinks = ()=>{
+        setShowLinks(!showLinks)
+    }
     return(
         <nav>
-            <ul className="nav-link">
-                <div className="nav-logo-link">
-                    <li className="nav-title">
-                        <a href=""><img src={logo}/></a>
-                    </li>
-                    <li className="nav-item no-dropdown">
-                        <a href="">Mes cartes</a>
-                    </li>
-                    <li className="no-dropdown">
-                        <a href="">Les vignerons partenaires</a>
-                    </li>
-                    <li className="no-dropdown">
-                        <a href="">Les animations</a>
-                    </li>
-                </div>
-                <div className="dropdown-container">
-                    <button className="btn-dropdown">
-                        <span className="material-symbols-outlined">menu</span>
-                    </button>
-
-                    <li className="dropdown">
-                        <button className="btn-flag">
-                            <img className="btn-img" src={franceFlag}/>
-                        </button>
-                        <div className="dropdown-flags">
-                            <button className="btn-flag-fr">
-                                <img className="btn-img-fr" src={franceFlag}/>
-                            </button>
-                            <button className="btn-flag-us">
-                                <img className="btn-img-us" src={usFlag}/>
-                            </button>
-                        </div>
-
-                    </li>
-                </div>
+            <a href=""><img src={logo}/></a>
+            <ul className={`nav-ul ${showLinks ? "show-nav" : "dont-show"} `}>
+                <li className="nav-link">
+                    <a href="">Mes cartes</a>
+                </li>
+                <li className="nav-link">
+                    <a href="">Les vignerons partenaires</a>
+                </li>
+                <li className="nav-link">
+                    <a href="">Les animations</a>
+                </li>
 
             </ul>
+            <button className="nav_burger" onClick={handleShowLinks} aria-label="Bouton ouvre la barre de navigation">
+                <span className="burger_bar"></span>
+            </button>
+            <div className="dropdown-container">
+                <button className="btn-dropdown">
+                    <span className="material-symbols-outlined">menu</span>
+                </button>
+
+                <li className="dropdown">
+                    <button className="btn-flag">
+                        <img className="btn-img" src={franceFlag}/>
+                    </button>
+                    <div className="dropdown-flags">
+                        <button className="btn-flag-fr">
+                            <img className="btn-img-fr" src={franceFlag}/>
+                        </button>
+                        <button className="btn-flag-us">
+                            <img className="btn-img-us" src={usFlag}/>
+                        </button>
+                    </div>
+
+                </li>
+            </div>
         </nav>
 
 )
