2e3c967d8d71a19891507ad691e5cefada1aa6f6
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-05 16:42:56 +0100

    Ajout d'un menu burger qui s'affiche que pour le mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,51 +3,34 @@ import  logo  from "/public/images/favicon/favicon-32x32.png"
 import  franceFlag  from "/public/images/flags/france.png"
 import  usFlag  from "/public/images/flags/united-states.png"
 import {useState} from "react";
+import "./Navbar.scss";
 
 function Navbar(){
-    const [showLinks, setShowLinks] = useState(false)
 
-    const handleShowLinks = ()=>{
-        setShowLinks(!showLinks)
-    }
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+
     return(
         <nav>
-            <a href=""><img src={logo}/></a>
-            <ul className={`nav-ul ${showLinks ? "show-nav" : "dont-show"} `}>
-                <li className="nav-link">
-                    <a href="">Mes cartes</a>
-                </li>
-                <li className="nav-link">
-                    <a href="">Les vignerons partenaires</a>
-                </li>
-                <li className="nav-link">
-                    <a href="">Les animations</a>
-                </li>
+            <a href="#" aria-label="Liens vers l'accueil"><img src={logo} alt="Logo"/></a>
+            <ul className={`nav-ul ${isOpen ? "show-nav" : "dont-show"} `}>
 
-            </ul>
-            <button className="nav_burger" onClick={handleShowLinks} aria-label="Bouton ouvre la barre de navigation">
-                <span className="burger_bar"></span>
-            </button>
-            <div className="dropdown-container">
-                <button className="btn-dropdown">
-                    <span className="material-symbols-outlined">menu</span>
+                <li><a href="" className="nav-link" onClick={toggleMenu}>Les cartes</a></li>
+                <li><a href="" className="nav-link" onClick={toggleMenu}>Les vignerons partenaires</a></li>
+                <li><a href="" className="nav-link" onClick={toggleMenu}>Les animations</a></li>
+                <li><a href="" className="nav-link" onClick={toggleMenu}>Déconnexion</a></li>
+                <button className="quit_button" onClick={toggleMenu} aria-label="Bouton ouvre la barre de navigation">
+                    <span className="quit"></span>
                 </button>
 
-                <li className="dropdown">
-                    <button className="btn-flag">
-                        <img className="btn-img" src={franceFlag}/>
-                    </button>
-                    <div className="dropdown-flags">
-                        <button className="btn-flag-fr">
-                            <img className="btn-img-fr" src={franceFlag}/>
-                        </button>
-                        <button className="btn-flag-us">
-                            <img className="btn-img-us" src={usFlag}/>
-                        </button>
-                    </div>
-
-                </li>
-            </div>
+            </ul>
+            <button className="nav_burger" onClick={toggleMenu} aria-label="Bouton ouvre la barre de navigation">
+                <span className="burger_bar"></span>
+            </button>
         </nav>
 
 )
