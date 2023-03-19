b420a0cd57abbfa94511d9f2fa411343bc110e86
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-19 16:28:32 +0100

    Ajout des méthodes login et logout

--- src/services/api/users.js
+++ src/services/api/users.js
@@ -1,9 +1,19 @@
 export const BASE_URL = "http://127.0.0.1:35729/api";
 export function getMe() {
-  return fetch(`${BASE_URL}/me`).then((response) => {
-    if (response.status === 401) {
-      return Promise.resolve(null);
+  return fetch(`${BASE_URL}/me`, { credentials: "include" }).then(
+    (response) => {
+      if (response.status === 401) {
+        return Promise.resolve(null);
+      }
+      return response.json();
     }
-    return response.json();
-  });
+  );
+}
+
+export function loginUrl() {
+  return `${BASE_URL}/login?redirect=${window.location}`;
+}
+
+export function logoutUrl() {
+  return new URL(`${BASE_URL}/logout`);
 }
