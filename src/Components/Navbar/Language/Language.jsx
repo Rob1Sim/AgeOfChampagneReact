2632889b7a7e5dbbf8ba9c4aab0b6d3792339bbe
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-23 18:47:05 +0100

    Avec l'utilisation de i18next ajout de la possibilité de changer la langue de la page

--- src/Components/Navbar/Language/Language.jsx
+++ src/Components/Navbar/Language/Language.jsx
@@ -3,10 +3,11 @@ import franceFlag from "../../../images/flags/france.png";
 import usFlag from "../../../images/flags/united-states.png";
 import CountryFlag from "./CountryFlag";
 import "./Language.scss";
+import { useTranslation } from "react-i18next";
 
 function Language() {
   const [language, setLanguage] = useState("fr");
-
+  const { i18n } = useTranslation();
   const [selectedLanguage, setSelectedLanguage] = useState(franceFlag);
   /**
    * Change la langue du pays au clique (drapeau, langue du site)
@@ -15,9 +16,11 @@ function Language() {
     if (selectedLanguage === franceFlag) {
       setSelectedLanguage(usFlag);
       setLanguage("us");
+      i18n.changeLanguage("en");
     } else {
       setSelectedLanguage(franceFlag);
       setLanguage("fr");
+      i18n.changeLanguage("fr");
     }
   };
   return (
