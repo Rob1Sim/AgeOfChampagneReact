73a55f9a32c1003b4d27583f32d01c317edbf276
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-03-24 11:30:31 +0100

    Code plus lisible

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -5,12 +5,11 @@ export function CardList() {
   const [cardData, setCardData] = useState([]);
 
   useEffect(() => {
-    fetchAllCards().then((data) =>
-      {
-        setCardData(data["hydra:member"]);
-      }
-    )
+    fetchAllCards().then((data) => {
+      setCardData(data["hydra:member"]);
+    });
   }, []);
 
+  // eslint-disable-next-line react/react-in-jsx-scope
   return cardData.map((card) => <div>{card.name}</div>);
 }
