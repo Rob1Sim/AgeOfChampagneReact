5f3636d5315f1e6763e1c521a04aa9bfb24d2bca
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-03-24 11:08:28 +0100

    Ajout de la méthode retournant l'image d'une carte

--- src/services/api/cards.js
+++ src/services/api/cards.js
@@ -3,3 +3,7 @@ export const BASE_URL = "https://127.0.0.1:8000/api";
 export function fetchAllCards() {
   return fetch(`${BASE_URL}/cartes`).then((response) => response.json());
 }
+
+export function cardImgUrl(cardId) {
+  return `${BASE_URL}/cartes/${cardId}`;
+}
