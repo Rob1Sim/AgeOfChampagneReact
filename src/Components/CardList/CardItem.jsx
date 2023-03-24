adb14094c5f5c58b5e0490d5526ca8f512b63d1a
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-03-24 11:03:13 +0100

    Affichage d'une carte

--- src/Components/CardList/CardItem.jsx
+++ src/Components/CardList/CardItem.jsx
@@ -1,1 +1,16 @@
+import { useState, useEffect } from "react";
+import { fetchAllCards } from "../../services/api/cards";
 
+export function BookmarksList() {
+  const [cardData, setCardData] = useState([]);
+
+  useEffect(() => {
+    fetchAllCards().then((data) =>
+      {
+        setCardData(data["hydra:member"]);
+      }
+    )
+  }, []);
+
+  return cardData.map((card) => <div>{card.name}</div>);
+}
