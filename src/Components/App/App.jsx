44581e9549472470f46ad11c9906035f7de1974e
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-03-24 12:10:54 +0100

    Utilisation de CardList dans App

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Provider from "../../contexts/user/Provider";
+import { CardList } from "../CardList/CardList";
 
 function App() {
   return (
     <Provider>
       <div className="App">
         <Navbar />
+        <CardList />
       </div>
     </Provider>
   );
