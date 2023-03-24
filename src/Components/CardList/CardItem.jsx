e15b5ca1b0159937676ae20d71cb2e7867b30f0c
breb0007 <noa.brebant@etudiant.univ-reims.fr>
2023-03-24 11:15:44 +0100

    Utilisation de propTypes dans CardItem

--- src/Components/CardList/CardItem.jsx
+++ src/Components/CardList/CardItem.jsx
@@ -1,16 +1,25 @@
-import { useState, useEffect } from "react";
-import { fetchAllCards } from "../../services/api/cards";
+import PropTypes from "prop-types";
+import { cardImgUrl } from "../../services/api/cards";
 
-export function BookmarksList() {
-  const [cardData, setCardData] = useState([]);
-
-  useEffect(() => {
-    fetchAllCards().then((data) =>
-      {
-        setCardData(data["hydra:member"]);
-      }
-    )
-  }, []);
-
-  return cardData.map((card) => <div>{card.name}</div>);
+export function CardItem({ data }) {
+  return (
+    <div>
+      <img src={cardImgUrl(data.contenuImage)} alt={data.nom} />
+    </div>
+  );
 }
+
+CardItem.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  data: PropTypes.shape({
+    id: PropTypes.number,
+    nom: PropTypes.string,
+    type: PropTypes.string,
+    region: PropTypes.string,
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+    superficie: PropTypes.number,
+    contenuImage: PropTypes.string,
+    cru_r: PropTypes.string,
+  }),
+};
