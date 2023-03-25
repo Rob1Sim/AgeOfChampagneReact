26c5597bdd0943526797ac94c5e534c1e85e7b4d
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-25 18:22:57 +0100

    Mis à jour du lien vers l'API

--- src/services/api/users.js
+++ src/services/api/users.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:8000/api";
+export const BASE_URL = "https://127.0.0.1:8000/api";
 export function getMe() {
   return fetch(`${BASE_URL}/me`, { credentials: "include" }).then(
     (response) => {
