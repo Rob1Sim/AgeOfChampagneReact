e8a90c8704d05eb0e617e3d5e36a02622d3850c0
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-26 19:13:45 +0200

    Correction avec eslint

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -2,6 +2,26 @@ import { React, useState, useEffect } from "react";
 import { fetchAllCards } from "../../services/api/cards";
 import { CardItem } from "./CardItem";
 
+function handleClick(card) {
+  const lastClickedCard = JSON.parse(
+    window.sessionStorage.getItem("lastClickedCards") || "[]"
+  );
+  const cardIndex = lastClickedCard.indexOf(card.id);
+  if (cardIndex !== -1) {
+    // Remove the card from its current position and add it at the beginning
+    lastClickedCard.splice(cardIndex, 1);
+    lastClickedCard.unshift(card.id);
+  } else {
+    lastClickedCard.unshift(card.id);
+    if (lastClickedCard.length > 10) {
+      lastClickedCard.pop();
+    }
+  }
+  window.sessionStorage.setItem(
+    "lastClickedCards",
+    JSON.stringify(lastClickedCard)
+  );
+}
 export function CardList() {
   const [cardData, setCardData] = useState([]);
   const [cardList, setCardList] = useState([]);
@@ -20,27 +40,6 @@ export function CardList() {
       );
     });
   }, []);
-
-  function handleClick(card) {
-    const lastClickedCard = JSON.parse(
-      window.sessionStorage.getItem("lastClickedCards") || "[]"
-    );
-    const cardIndex = lastClickedCard.indexOf(card.id);
-    if (cardIndex !== -1) {
-      // Remove the card from its current position and add it at the beginning
-      lastClickedCard.splice(cardIndex, 1);
-      lastClickedCard.unshift(card.id);
-    } else {
-      lastClickedCard.unshift(card.id);
-      if (lastClickedCard.length > 10) {
-        lastClickedCard.pop();
-      }
-    }
-    window.sessionStorage.setItem(
-      "lastClickedCards",
-      JSON.stringify(lastClickedCard)
-    );
-  }
 
   const lastClickedCardsJson =
     window.sessionStorage.getItem("lastClickedCards");
