c37813f84b26ae1f3fab5962f00e9165c7783acf
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-26 14:59:11 +0200

    Affichage pour l'instant de seulement une image pour une animation

--- src/Components/AnimationList/AnimationItem.jsx
+++ src/Components/AnimationList/AnimationItem.jsx
@@ -1,1 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { animationImgUrl } from "../../services/api/animations";
 
+export function AnimationItem({ data }) {
+  return (
+    <div>
+      <img src={animationImgUrl(data.id)} alt={data.nom} />
+    </div>
+  );
+}
+
+AnimationItem.defaultProps = {
+  data: [],
+};
+
+AnimationItem.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.number,
+    nom: PropTypes.string,
+    type: PropTypes.string,
+    horaireDeb: PropTypes.instanceOf(Date),
+    horaireFin: PropTypes.instanceOf(Date),
+    prix: PropTypes.number,
+    contenuImage: PropTypes.string,
+  }),
+};
+
+export default AnimationItem;
