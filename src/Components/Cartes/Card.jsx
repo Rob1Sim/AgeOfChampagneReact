f38877399becdfcd2b532b7028a497e7c327260f
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-26 16:09:45 +0200

    Ajout de la récupération du cru pour une carte

--- src/Components/Cartes/Card.jsx
+++ src/Components/Cartes/Card.jsx
@@ -1,30 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useRoute } from "wouter";
 import { useTranslation } from "react-i18next";
-import { fetchCard, getCardImage } from "../../services/api/card";
+import {
+  fetchCard,
+  fetchCruFromCard,
+  getCardImage,
+} from "../../services/api/card";
+import Loading from "../Loading";
 
 function Card() {
   const [, { cardId }] = useRoute("/cartes/:cardId");
   const { t } = useTranslation("card");
   const [card, setCard] = useState();
   const [, setLocation] = useLocation();
-
+  const [cru, setCru] = useState();
   useEffect(() => {
     setCard(undefined);
+    setCru(undefined);
     if (Number.isInteger(parseInt(cardId, 10))) {
       fetchCard(cardId).then((response) => {
         if (response === null) {
           setLocation("/cartes");
         }
         setCard(response);
+        // Récupération du cru à partir de la relation
+        fetchCruFromCard(response.cru_r).then((cruResponse) => {
+          if (cruResponse === null) {
+            setCru(undefined);
+          }
+          setCru(cruResponse);
+        });
       });
     }
   }, [cardId]);
 
   return (
     <div>
-      <img src={getCardImage(cardId)} alt={t("alt-card")} />
-      {card.name}
+      {card === undefined ? (
+        <Loading />
+      ) : (
+        <div>
+          <img src={getCardImage(cardId)} alt={t("alt-card")} />
+          {card.nom}
+          {card.type}
+          {card.region}
+          {cru === undefined ? (
+            <p>Pas de cru pour cette carte</p>
+          ) : (
+            <div>
+              {cru.libelle}
+              {cru.infos}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
