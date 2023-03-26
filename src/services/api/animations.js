a62556c10639301a4dbcc5d5ae3a3dd9518a16ce
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-26 14:14:38 +0200

    Création de la fonction pour récupérer l'image de l'animation

--- src/services/api/animations.js
+++ src/services/api/animations.js
@@ -3,3 +3,7 @@ export const BASE_URL = "https://127.0.0.1:8000/api";
 export function fetchAllAnimations() {
   return fetch(`${BASE_URL}/animations`).then((response) => response.json());
 }
+
+export function animationImgUrl(animationId) {
+  return `${BASE_URL}/animations/${animationId}/image`;
+}
