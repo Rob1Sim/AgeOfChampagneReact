f38b8fb7df8af7eb4634aeb4f4f4215b186ebfc7
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-26 15:45:58 +0200

    Ajout d'un useEffet qui vérifie si la carte existe et charge la page si oui, sinon il redirige vers /cartes

--- src/Components/Cartes/Card.jsx
+++ src/Components/Cartes/Card.jsx
@@ -1,9 +1,32 @@
-import React from "react";
-import { useRoute } from "wouter";
+import React, { useEffect, useState } from "react";
+import { useLocation, useRoute } from "wouter";
+import { useTranslation } from "react-i18next";
+import { fetchCard, getCardImage } from "../../services/api/card";
 
-const Card = () => {
+function Card() {
   const [, { cardId }] = useRoute("/cartes/:cardId");
+  const { t } = useTranslation("card");
+  const [card, setCard] = useState();
+  const [, setLocation] = useLocation();
 
-};
+  useEffect(() => {
+    setCard(undefined);
+    if (Number.isInteger(parseInt(cardId, 10))) {
+      fetchCard(cardId).then((response) => {
+        if (response === null) {
+          setLocation("/cartes");
+        }
+        setCard(response);
+      });
+    }
+  }, [cardId]);
+
+  return (
+    <div>
+      <img src={getCardImage(cardId)} alt={t("alt-card")} />
+      {card.name}
+    </div>
+  );
+}
 
 export default Card;
