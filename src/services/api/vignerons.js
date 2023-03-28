dc7a018c01aa9ca08a1639f7574f2522736e331d
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-28 15:27:41 +0200

    Création de la fonction pour récupérer le cru associé au vigneron

--- src/services/api/vignerons.js
+++ src/services/api/vignerons.js
@@ -7,3 +7,20 @@ export function fetchAllVignerons() {
 export function vigneronImgUrl(vigneronId) {
   return `${BASE_URL}/vignerons/${vigneronId}/image`;
 }
+
+/**
+ * Récupère le cru associé à un vigneron grâce à son ID
+ * @param cruId
+ * @returns {Promise<Response>}
+ */
+export function fetchCruFromVigneron(cruId) {
+  return fetch(`${BASE_URL}/crus/${cruId}`, {
+    credentials: "include",
+  }).then((response) => {
+    if (!response.ok) {
+      return Promise.resolve(null);
+    }
+    return response.json();
+  });
+}
+
