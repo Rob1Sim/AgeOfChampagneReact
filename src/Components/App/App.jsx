fe4c3d92468238e6f8eaf960de9157247e24311f
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-28 11:31:11 +0200

    Ajout du provider à App

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,33 +5,36 @@ import Navbar from "../Navbar/Navbar";
 import Provider from "../../contexts/user/Provider";
 import CardList from "../CardList/CardList";
 import Card from "../Cartes/Card";
+import BurgerButtonProvider from "../../contexts/burgerButton/burgerButton";
 
 function App() {
   return (
     <Provider>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/">
-            <Redirect to="/cartes" />
-          </Route>
-          <Route path="/cartes">
-            <CardList />
-          </Route>
-          <Route path="/cartes/:cardId">
-            <Card />
-          </Route>
-          <Route path="/animations">
-            <h1>Les animations</h1>
-          </Route>
-          <Route path="/partenaires">
-            <h1>Les partenaires</h1>
-          </Route>
-          <Route path="/profile">
-            <h1>Votre profile</h1>
-          </Route>
-        </Switch>
-      </div>
+      <BurgerButtonProvider>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/">
+              <Redirect to="/cartes" />
+            </Route>
+            <Route path="/cartes">
+              <CardList />
+            </Route>
+            <Route path="/cartes/:cardId">
+              <Card />
+            </Route>
+            <Route path="/animations">
+              <h1>Les animations</h1>
+            </Route>
+            <Route path="/partenaires">
+              <h1>Les partenaires</h1>
+            </Route>
+            <Route path="/profile">
+              <h1>Votre profile</h1>
+            </Route>
+          </Switch>
+        </div>
+      </BurgerButtonProvider>
     </Provider>
   );
 }
