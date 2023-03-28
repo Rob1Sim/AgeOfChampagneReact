346ac7c4935e8a4b80148cb517313ee8626ff590
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-28 15:16:49 +0200

    Création de la fonction pour récupérer l'image du vigneron

--- src/services/api/vignerons.js
+++ src/services/api/vignerons.js
@@ -3,3 +3,7 @@ export const BASE_URL = "https://127.0.0.1:8000/api";
 export function fetchAllVignerons() {
   return fetch(`${BASE_URL}/vignerons`).then((response) => response.json());
 }
+
+export function vigneronImgUrl(vigneronId) {
+  return `${BASE_URL}/vignerons/${vigneronId}/image`;
+}
