9a7def1dbc3ff8837aebf6b953661ff96e4e3fc6
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-28 10:19:13 +0200

    Ajout d'une méthode pour récupérer le vignerons associé à un cru

--- src/services/api/cards.js
+++ src/services/api/cards.js
@@ -42,3 +42,19 @@ export function fetchCruFromCard(cruLink) {
     return response.json();
   });
 }
+
+/**
+ * Récupère le vignerons associé à une carte
+ * @param wineMakerId
+ * @returns {Promise<Response>}
+ */
+export function fetchWineMakerFromCard(wineMakerId) {
+  return fetch(`${BASE_URL}/vignerons/${wineMakerId}`, {
+    credentials: "include",
+  }).then((response) => {
+    if (!response.ok) {
+      return Promise.resolve(null);
+    }
+    return response.json();
+  });
+}
