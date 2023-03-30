1efdfc025b943dabeb9f98519ed927bb80b159cd
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-03-30 13:52:41 +0200

    Ajout de la condition sur le fetch : Permet de savoir si le fetch retourne quelque chose ou non

--- src/Components/VigneronList/VigneronList.jsx
+++ src/Components/VigneronList/VigneronList.jsx
@@ -5,20 +5,31 @@ import VigneronItem from "./VigneronItem";
 export function VigneronList() {
   const [vigneronData, setVigneronData] = useState([]);
   const [vigneronList, setVigneronList] = useState([]);
+  const [isDataAvailable, setIsDataAvailable] = useState(true);
 
   useEffect(() => {
-    fetchAllVignerons().then((data) => {
-      setVigneronData(data["hydra:member"]);
-      setVigneronList(
-        data["hydra:member"].map((vigneron) => (
-          <VigneronItem
-            key={vigneron.id}
-            data={vigneron}
-            onClick={() => onclick(vigneron)}
-          />
-        ))
-      );
-    });
+    fetchAllVignerons()
+      .then((data) => {
+        // Si le fetch retourne quelque chose, crée un composant VigneronItem
+        if (data["hydra:member"].length > 0) {
+          setVigneronData(data["hydra:member"]);
+          setVigneronList(
+            data["hydra:member"].map((vigneron) => (
+              <VigneronItem
+                key={vigneron.id}
+                data={vigneron}
+                onClick={() => onclick(vigneron)}
+              />
+            ))
+          );
+          // Sinon le fetch ne retourne rien, isDataAvailable vaut false
+        } else {
+          setIsDataAvailable(false);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching winemaker:", error);
+      });
   }, []);
 
   // eslint-disable-next-line react/react-in-jsx-scope,react/jsx-no-useless-fragment
