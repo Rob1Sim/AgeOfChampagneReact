6f954dd3f69ac021b7742e6a729996154ea9a5af
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-31 11:32:33 +0200

    Ajout de la traduction au message d'erreur

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Error.scss";
+import { useTranslation } from "react-i18next";
 import lost from "../../images/lost.svg";
 
 function Error() {
+  const { t } = useTranslation("error");
   return (
     <div className="error">
       <h1>404</h1>
-      <h2>Oups ! La page que vous recherchez n&apos;existe pas</h2>
-      <img src={lost} alt="Une personne qui est perdue" />
+      <h2>{t("error-message")}</h2>
+      <img src={lost} alt={t("alt-img")} />
     </div>
   );
 }
