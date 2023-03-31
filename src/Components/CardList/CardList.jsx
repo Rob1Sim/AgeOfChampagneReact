cdd24e0ca51cc61ff79c30e683b5fd1aa77f81b8
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-03-31 15:22:04 +0200

    Ajout d'une méthode appelé lors du changement de catégory qui relance une requete pour récupérer les cartes trier celon l'api

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useEffect, useContext } from "react";
 import { useTranslation } from "react-i18next";
-import { fetchAllCards } from "../../services/api/cards";
+import { fetchAllCards, getCardWithFilter } from "../../services/api/cards";
 import CardItem from "./CardItem";
 import { handleClick } from "../../hooks/cards/cardsClick";
 import "./CardList.scss";
@@ -11,6 +11,7 @@ function CardList() {
   const [cardData, setCardData] = useState([]);
   const [cardList, setCardList] = useState(undefined);
   const [searchParams, setSearchParams] = useState("");
+  const [category, setCategory] = useState("");
   const [, setIsDataAvailable] = useState(true);
   const { t } = useTranslation("cardlist");
   const { opened } = useContext(BugerButtonContext);
@@ -43,6 +44,32 @@ function CardList() {
       });
   }, [searchParams]);
 
+  useEffect(() => {
+    getCardWithFilter(category)
+      .then((dataFilter) => {
+        // Si le fetch retourne quelque chose, crée un composant CardItem
+        if (dataFilter["hydra:member"].length > 0) {
+          setCardData(dataFilter["hydra:member"]);
+          setCardList(
+            dataFilter["hydra:member"].map((card) => (
+              <CardItem
+                className="cards"
+                key={card.id}
+                data={card}
+                onClick={() => handleClick(card)}
+              />
+            ))
+          );
+          // Si le fetch ne retourne rien, isDataAvailable vaut false
+        } else {
+          setIsDataAvailable(false);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching cards:", error);
+      });
+  }, [category]);
+
   const lastClickedCardsJson =
     window.sessionStorage.getItem("lastClickedCards");
   let lastClickedCards = [];
@@ -60,6 +87,15 @@ function CardList() {
     setSearchParams(event.target.value);
   };
 
+  const handleSelectChange = (event) => {
+    if (event.target.value !== "category") {
+      console.log(event.target.value);
+      setCategory(event.target.value);
+    } else {
+      setCategory("");
+    }
+  };
+
   return (
     <div className={opened ? "display-on" : "CardList"}>
       <form>
@@ -70,13 +106,17 @@ function CardList() {
           value={searchParams}
           onChange={handleSearchInputChange}
         />
-        <select className="dropdown-category" name="category">
-          <option value defaultValue="Catégorie">
+        <select
+          className="dropdown-category"
+          name="category"
+          onChange={handleSelectChange}
+        >
+          <option value defaultValue="category">
             Catégorie
           </option>
-          <option value={1}>Vignerons</option>
-          <option value={2}>Cru</option>
-          <option value={3}>Région</option>
+          <option value="nom">Nom</option>
+          <option value="type">Type</option>
+          <option value="region">Région</option>
         </select>
       </form>
 
