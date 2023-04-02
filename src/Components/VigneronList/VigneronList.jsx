a529e4f316ebf3541104d11afd325fab408c69cc
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-04-02 14:46:24 +0200

    Retrait de lignes de codes pour le mettre dans un autre composant

--- src/Components/VigneronList/VigneronList.jsx
+++ src/Components/VigneronList/VigneronList.jsx
@@ -1,24 +1,15 @@
 import { useState, useEffect } from "react";
-import {
-  fetchAllVignerons,
-  fetchCruFromVigneron,
-  fetchProduitFromVigneron,
-} from "../../services/api/vignerons";
+import { fetchAllVignerons } from "../../services/api/vignerons";
 import VigneronItem from "./VigneronItem";
 import Loading from "../Loading/Loading";
-import Error from "../Error/Error";
 
 export function VigneronList() {
   const [vigneronData, setVigneronData] = useState([]);
   const [vigneronList, setVigneronList] = useState([]);
-  const [cru, setCru] = useState();
-  const [produit, setProduit] = useState();
   const [isDataAvailable, setIsDataAvailable] = useState(true);
 
   useEffect(() => {
     setIsDataAvailable(true);
-    setProduit(undefined);
-    setCru(undefined);
     setVigneronList(undefined);
     fetchAllVignerons()
       .then((data) => {
@@ -38,21 +29,6 @@ export function VigneronList() {
         } else {
           setIsDataAvailable(false);
         }
-        // Récupération du cru à partir de la relation vigneron
-        fetchCruFromVigneron(data.cruID).then((cruResponse) => {
-          if (cruResponse === null) {
-            setCru(undefined);
-          }
-          setCru(cruResponse);
-        });
-
-        // Récupération du produit à partir de la relation vigneron
-        fetchProduitFromVigneron(data.produitID).then((produitResponse) => {
-          if (produitResponse === null) {
-            setProduit(undefined);
-          }
-          setProduit(produitResponse);
-        });
       })
       .catch((error) => {
         console.error("Error fetching winemaker:", error);
