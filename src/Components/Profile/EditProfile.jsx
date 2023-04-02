3f65760fa568fc9535a872bf41eb773b232d58ea
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-04-02 11:01:28 +0200

    Ajout de la méthode handleSubmit

--- src/Components/Profile/EditProfile.jsx
+++ src/Components/Profile/EditProfile.jsx
@@ -1,14 +1,20 @@
 import React, { useContext, useState } from "react";
 import UserContext from "../../contexts/user/index";
 
-
 function EditProfile() {
   const { userData } = useContext(UserContext);
   const [userLogin, setUserLogin] = useState(userData ? userData.login : "");
   const [userEmail, setUserEmail] = useState(userData ? userData.email : "");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (userLogin !== "" && userEmail !== "") {
+      // TODO Patch l'utilisateur courant
+    }
+  };
+
   return (
-    <form onSubmit={}>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="name">Identifiant :</label>
         <input
