9b8de531c4346786b6e0aee63c030f943fd36bc8
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-04-02 10:58:45 +0200

    Ajout du composant EditProfile

---
+++ src/Components/Profile/EditProfile.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from "react";
+import UserContext from "../../contexts/user/index";
+
+
+function EditProfile() {
+  const { userData } = useContext(UserContext);
+  const [userLogin, setUserLogin] = useState(userData ? userData.login : "");
+  const [userEmail, setUserEmail] = useState(userData ? userData.email : "");
+
+  return (
+    <form onSubmit={}>
+      <div>
+        <label htmlFor="name">Identifiant :</label>
+        <input
+          type="text"
+          id="name"
+          value={userLogin}
+          onChange={(e) => setUserLogin(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email :</label>
+        <input
+          type="text"
+          id="email"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <button type="submit">Modifier</button>
+      </div>
+    </form>
+  );
+}
+
+export default EditProfile;
