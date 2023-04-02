7c53e7ed6e53c09a593a49a2e875ee731a28596b
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-04-02 11:56:12 +0200

    Modification de profile pour mettre un bouton d'édition qui active le formulaire quand on clique dessus et le désactive quand on reclick

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import EditProfile from "./EditProfile";
+import UserContext from "../../contexts/user/index";
 
 function Profile() {
-  return <EditProfile />;
+  const { userData } = useContext(UserContext);
+  const [isEditing, setEditing] = useState(false);
+  return (
+    <main>
+      <h1>Bonjour {userData.login}</h1>
+      {isEditing ? <EditProfile userData={userData} /> : ""}
+      <button type="button" onClick={() => setEditing(!isEditing)}>
+        {isEditing ? "Annuler" : "Modifier son profil"}
+      </button>
+    </main>
+  );
 }
 
 export default Profile;
