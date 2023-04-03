e13b2e1f106b36f2ad4f69d268541a870e914c68
Roman Szczepaniak <roman.szczepaniak@etudiant.univ-reims.fr>
2023-04-03 13:27:20 +0200

    Rajout d'une condition dans la liste des animations : Permet de savoir si le fetch retourne qqch ou non

--- src/Components/AnimationList/AnimationList.jsx
+++ src/Components/AnimationList/AnimationList.jsx
@@ -5,20 +5,32 @@ import AnimationItem from "./AnimationItem";
 function AnimationList() {
   const [, setAnimationData] = useState([]);
   const [animationList, setAnimationList] = useState([]);
+  const [, setIsDataAvailable] = useState(true);
 
   useEffect(() => {
-    fetchAllAnimations().then((data) => {
-      setAnimationData(data["hydra:member"]);
-      setAnimationList(
-        data["hydra:member"].map((animation) => (
-          <AnimationItem
-            key={animation.id}
-            data={animation}
-            onClick={() => onclick(animation)}
-          />
-        ))
-      );
-    });
+    setIsDataAvailable(true);
+    fetchAllAnimations()
+      .then((data) => {
+        // Si le fetch retourne quelque chose, crée un composant AnimationItem
+        if (data["hydra:member"].length > 0) {
+          setAnimationData(data["hydra:member"]);
+          setAnimationList(
+            data["hydra:member"].map((animation) => (
+              <AnimationItem
+                key={animation.id}
+                data={animation}
+                onClick={() => onclick(animation)}
+              />
+            ))
+          );
+          // Sinon le fetch ne retourne rien, isDataAvailable vaut false
+        } else {
+          setIsDataAvailable(false);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching animation:", error);
+      });
   }, []);
 
   // eslint-disable-next-line react/jsx-no-useless-fragment,react/react-in-jsx-scope
