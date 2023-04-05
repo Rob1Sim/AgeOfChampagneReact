dfc5f7378eef1733eab50b4b1ceb7043578b1710
Erwann Maton <erwann.maton@etudiant.univ-reims.fr>
2023-04-05 09:48:42 +0200

    Creation du fichier json pour la traduction du profile et ajout de la constante de traduction dans le profle

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,10 +1,12 @@
 import React, { useContext, useState } from "react";
+import { useTranslation } from "react-i18next";
 import EditProfile from "./EditProfile";
 import UserContext from "../../contexts/user/index";
 
 function Profile() {
   const { userData } = useContext(UserContext);
   const [isEditing, setEditing] = useState(false);
+  const { t } = useTranslation("editProfile");
   return (
     <main>
       <h1>Bonjour {userData.login}</h1>
