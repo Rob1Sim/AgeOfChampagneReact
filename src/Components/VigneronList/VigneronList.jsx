3d296befd967cecac9750f3d9b5ce531b087946c
simo0156 <robin.simonneau@etudiant.univ-reims.fr>
2023-04-06 21:57:49 +0200

    Ajout du bloquage de contenue lorsque le menu burger est ouvert

--- src/Components/VigneronList/VigneronList.jsx
+++ src/Components/VigneronList/VigneronList.jsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { fetchAllVignerons } from "../../services/api/vignerons";
 import VigneronItem from "./VigneronItem";
 import Loading from "../Loading/Loading";
 import "./VigneronList.scss";
+import BugerButtonContext from "../../contexts/burgerMenu/index";
 
 export function VigneronList() {
-  const [vigneronData, setVigneronData] = useState([]);
+  const [, setVigneronData] = useState([]);
   const [vigneronList, setVigneronList] = useState([]);
-  const [isDataAvailable, setIsDataAvailable] = useState(true);
+  const [, setIsDataAvailable] = useState(true);
   const { t } = useTranslation("vigneronslist");
+  const { opened } = useContext(BugerButtonContext);
 
   useEffect(() => {
     setIsDataAvailable(true);
@@ -39,7 +41,7 @@ export function VigneronList() {
   }, []);
 
   return (
-    <div className="wineMaker">
+    <div className={opened ? "display-on" : "wineMaker"}>
       <h2>{t("list-winemakers")}</h2>
       {vigneronList !== undefined && vigneronList !== null ? (
         <div className="list-all-winemakers">{vigneronList}</div>
